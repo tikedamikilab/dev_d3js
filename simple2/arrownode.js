654db4dac0a8bb885eef69e99d930f51fa4d64cd
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-08-03 06:53:06 +0900

    線書けた

--- simple2/arrownode.js
+++ simple2/arrownode.js
@@ -4,9 +4,40 @@ function createsvg() {
     const contents = d3.select('#example');
     const svg = contents.append("svg");
 
+    let dataset = [
+        {
+            x : 100,
+            y : 70,
+        },
+        {
+            x : 200,
+            y : 30,
+        },
+        {
+            x : 300,
+            y : 60,
+        },
+    ]
+
     width = contents.node().clientWidth;
     height = contents.node().clientHeight;
 
-    
+    let color = d3.rgb("#85a7cc");
+
+    line = d3.line()
+        // lineのX軸をセット
+        .x(function(d) { return d.x; })
+        // lineのY軸をセット
+        .y(function(d) { return d.y; });
+
+    path = svg.append("path")
+        // dataをセット
+        .datum(dataset)
+        // 塗りつぶしをなしに
+        .attr("fill", "none")
+        // strokeカラーを設定
+        .attr("stroke", color)
+        // d属性を設定
+        .attr("d", line(dataset));
 
 };
