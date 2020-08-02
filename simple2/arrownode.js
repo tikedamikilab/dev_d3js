68105e6cb9014d47413b9ba435f296cb7d26731c
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-08-03 03:13:50 +0900

    arrownode

--- simple2/arrownode.js
+++ simple2/arrownode.js
@@ -1,26 +1,12 @@
 createsvg();
 
 function createsvg() {
-    var svg = d3.select("#example").append("svg")
-        .attr({
-            width: 640,
-            height: 480,
-        });
+    const contents = d3.select('#example');
+    const svg = contents.append("svg");
 
-    var c1 = [100, 90, 30];
-    var c2 = [200, 120, 20];
-    var carray = [c1, c2];
+    width = contents.node().clientWidth;
+    height = contents.node().clientHeight;
 
-    // line関数を定義 (x,y)は配列の[0],[1]とする。
-    var line = d3.svg.line()
-        .x(function (d) { return d[0]; })
-        .y(function (d) { return d[1]; });
+    
 
-    // path要素を作成
-    var path = svg.append('path')
-        .attr({
-            'd': line(carray),
-            'stroke': 'lightgreen',
-            'stroke-width': 5,
-        });
 };
