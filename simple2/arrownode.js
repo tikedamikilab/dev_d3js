6dfb8758cc1f2b06c36ff02ff11d4f6c97441784
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-08-03 17:44:09 +0900

    2 line

--- simple2/arrownode.js
+++ simple2/arrownode.js
@@ -2,43 +2,54 @@ createsvg();
 
 function createsvg() {
     const contents = d3.select('#example');
-    const svg = contents.append("svg");
+    const svg = contents.append("svg")
+            .attr("viewBox", "0 0 1000 1000")
+    
 
     let dataset = [
-        {
-            x : 100,
-            y : 70,
-        },
-        {
-            x : 200,
-            y : 30,
-        },
-        {
-            x : 300,
-            y : 60,
-        },
+        [
+            {
+                x : 100,
+                y : 70,
+            },
+            {
+                x : 300,
+                y : 300,
+            }
+        ],
+        [
+            {
+                x : 100,
+                y : 70,
+            },
+            {
+                x : 400,
+                y : 100,
+            }
+        ]
     ]
-
-    width = contents.node().clientWidth;
-    height = contents.node().clientHeight;
 
     let color = d3.rgb("#85a7cc");
 
+    // 描画の軸を決定？消すと上手くいかない
     line = d3.line()
         // lineのX軸をセット
         .x(function(d) { return d.x; })
         // lineのY軸をセット
         .y(function(d) { return d.y; });
 
-    path = svg.append("path")
-        // dataをセット
-        .datum(dataset)
-        // 塗りつぶしをなしに
-        .attr("fill", "none")
-        // strokeカラーを設定
-        .attr("stroke", color)
-        .attr("stroke-width", 10)
-        // d属性を設定
-        .attr("d", line(dataset));
+    for (let i = 0; i < dataset.length; i++){
+        // path全体の設定
+        path = svg.append("path")
+            // 塗りつぶしをなしに
+            .attr("fill", "none")
+            // strokeカラーを設定
+            .attr("stroke", color)
+            // stroke幅
+            .attr("stroke-width", 10)
+            // d属性を設定
+            .attr("d", line(dataset[i]));
+    }
+    
 
 };
