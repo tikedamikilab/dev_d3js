fb323d2ced26cb3770f0a901057bf6d12b8b213d
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-08-04 06:46:55 +0900

    bubble and arrow

--- bubbleAndArrow/bubble.js
+++ bubbleAndArrow/bubble.js
@@ -8,22 +8,34 @@ function createBubble() {
   // 1. 描画用のデータ準備
     const width = 300;
     const height = width;
-    const data = {
-        "name": "A",
-        "children": [
-        { "name": "B", "value": 25 },
-        { "name": "C", "value": 40 },
-        { "name": "D", "value": 60 }
-        ]
-    };
+
+    const data = [
+        {
+            "name": "A",
+            "children": [
+                { "name": "B", "value": 25 },
+                { "name": "C", "value": 40 },
+                { "name": "D", "value": 10 }
+            ]
+        },
+        {
+            "name": "E",
+            "children": [
+                { "name": "F", "value": 25 },
+                { "name": "G", "value": 40 },
+                { "name": "H", "value": 60 }
+            ]
+        },
+    ]
 
     const contents = d3.select('#bubble');
     const svg = contents.append("svg")
             .attr("width", width)
             .attr("height", height)
+            .attr("transform", "translate(50,50)");
 
     // 2. 描画用のデータ変換
-    root = d3.hierarchy(data);
+    root = d3.hierarchy(data[0]);
     root.sum(function(d) { return d.value; });
     
     const pack = d3.pack()
